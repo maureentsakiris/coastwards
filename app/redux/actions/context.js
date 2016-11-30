d9286658d3ae04dbbd7e2c52d0f445b268590da2
Momo <tsakiris@geographie.uni-kiel.de>
2016-11-30 19:05:37 +0100

    scrollToDiv preventDefault fix

--- app/redux/actions/context.js
+++ app/redux/actions/context.js
@@ -1,15 +1,8 @@
 /*import * as types from 'types'*/
 
-export function scrollToId ( id ){
+export function scrollToId ( id, e ){
 
-	/*window.scroll( {
-
-		top: document.body.scrollHeight, 
-		left: 0, 
-		behavior: 'smooth' 
-
-	} )*/
-
+	e.preventDefault()
 	let div = document.getElementById( id )
 	div.scrollIntoView( { behavior: 'smooth' } )
 
