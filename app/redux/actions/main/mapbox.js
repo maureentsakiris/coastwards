be510cce8aada5c82a79ea2b00989f18ff4c3d7a
Momo <info@maureentsakiris.com>
2017-11-12 12:02:17 +0100

    Stops

--- app/redux/actions/main/mapbox.js
+++ app/redux/actions/main/mapbox.js
@@ -4,11 +4,12 @@ import { promiseInitMapbox, mapboxPopup, mapboxLngLatConvert, mapboxNavigationCo
 import { isNull, findWhere, pluck, map } from 'underscore'
 import { promiseGet, promiseJSONOK } from 'actions/util/request/get'
 //import { resetMain } from 'actions/main/main'
-//import Modernizr from 'modernizr'
+import Modernizr from 'modernizr'
 
 const _map = map
 
 const CENTER = [ 0, 39 ]
+const STOPS = Modernizr.touch ? [ [ 0, 5 ], [ 10, 15 ], [ 22, 50 ] ] : [ [ 0, 4 ], [ 5, 6 ], [ 10, 15 ], [ 22, 50 ] ]
 const ZOOM = 1
 const ACCESSTOKEN = 'pk.eyJ1IjoibWF1cmVlbnRzYWtpcmlzIiwiYSI6ImNpanB0NzgwMjAxZDB0b2tvamNpYXQyeTMifQ.HVQAxH-RQKZBss1u3zIoxA'
 const OPTIONS = {
@@ -169,7 +170,7 @@ export const displayMap = ( ) => {
 					paint: {
 						'circle-radius': {
 							'base': 1.75,
-							'stops': [ [ 0, 5 ], [ 10, 15 ], [ 22, 50 ] ]
+							'stops': STOPS
 						},
 						'circle-color': {
 							property: 'materialverified',
