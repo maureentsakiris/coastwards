66687168a599d617f04ccd9048a028aa69b6a914
Matthias Buchholz <mbuchholz@babbel.com>
2023-03-25 14:42:18 +0100

    migrate to webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 //https://stackoverflow.com/questions/42286331/how-to-get-react-hot-loader-working-with-webpack-2-and-webpackdevmiddleware
 
 const webpack = require( 'webpack' )
-const ExtractTextPlugin = require( 'extract-text-webpack-plugin' )
-const { parsed: localEnv } = require( 'dotenv' ).config()
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const { parsed: localEnv = {} } = require( 'dotenv' ).config()
 //const CompressionPlugin = require( 'compression-webpack-plugin' )
 //const BundleAnalyzerPlugin = require( 'webpack-bundle-analyzer' ).BundleAnalyzerPlugin
 const { resolve } = require( 'path' )
@@ -20,6 +20,7 @@ const STYLES = resolve( __dirname, 'app/styles/' )
 module.exports = {
 
 	//devtool: 'nosources-source-map',
+	mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
 
 	stats: {
 
@@ -56,7 +57,11 @@ module.exports = {
 		alias: {
 			modernizr$:  resolve( __dirname, '.modernizrrc' )
 		},
-		extensions: [ '.js', '.jsx', '.scss' ]
+		extensions: [ '.js', '.jsx', '.scss' ],
+		fallback: {
+			url: require.resolve( 'url/' ),
+			http: require.resolve( "stream-http" )
+		}
 
 	},
 
@@ -73,7 +78,6 @@ module.exports = {
 				use: [ 
 
 					{ loader: 'babel-loader', options: { cacheDirectory: true } },
-					{ loader: 'eslint-loader' } 
 
 				]
 
@@ -81,13 +85,12 @@ module.exports = {
 			{
 
 				test: /\.modernizrrc$/,
-				use: [ 
-
-					{ loader: 'modernizr-loader' },
-					{ loader: 'json-loader' } 
-
-				]
-
+				use: [ {
+					loader: 'val-loader',
+					options: {
+						executableFile: require.resolve( 'val-loader-modernizr' )
+					}
+				} ]
 			},
 			{ 
 
@@ -96,7 +99,7 @@ module.exports = {
 				use: [ 
 
 					{ loader: 'url-loader', options: { limit: 10000 } },
-					{ loader: 'img-loader', options: { progressive: true } }
+					{ loader: 'file-loader', options: { progressive: true } }
 					
 				]
 
@@ -105,19 +108,12 @@ module.exports = {
 
 				test: /\.scss$/,
 				include: APP,
-				use: ExtractTextPlugin.extract( { 
-
-					fallback: "style-loader",
-					use: [
-
-						{ loader: 'css-loader', options: { modules: true, importLoaders: 1, localIdentName: '[name]_[local]' } },
-						{ loader: 'postcss-loader' },
-						{ loader: 'sass-loader' }
-
-					]
-
-				} )
-
+				use: [
+					MiniCssExtractPlugin.loader,
+					{ loader: 'css-loader', options: { modules: true, importLoaders: 1, localIdentName: '[name]_[local]' } },
+					{ loader: 'postcss-loader' },
+					{ loader: 'sass-loader' }
+				]
 			}
 
 		]
@@ -136,15 +132,9 @@ module.exports = {
 			debug: false
 
 		} ),
-		new webpack.optimize.CommonsChunkPlugin( {
-
-			names: [ 'vendor', 'manifest' ]
-
-		} ),
-		new ExtractTextPlugin( {
+		new MiniCssExtractPlugin( {
 
 			filename: "[name].css",
-			allChunks: true 
 
 		} ),
 		new webpack.EnvironmentPlugin( localEnv )
